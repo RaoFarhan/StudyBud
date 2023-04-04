db961f7b75120e0ec9a1d4c77d285dd3233ad307
Farhan Yaqoob <farhan8418g@gmail.com>
2023-04-04 09:51:32 +0200

    Studybud Source Code

---
+++ studybud/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
+
+    
+]
